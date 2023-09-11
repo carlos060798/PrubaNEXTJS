9cd470732667d79ec833ecbd510510b7baccd209
carlos danilo angarita <daniloangaritagarcia@gmail.com>
2023-09-11 16:40:20 -0500

    version produccion

--- src/app/post/[id]/page.jsx
+++ src/app/post/[id]/page.jsx
@@ -6,14 +6,14 @@ async function PostPage({ params }) {
   
   return (
     <>
-        <h1 className="text-2xl font-bold mb-4">Detalle de la publicación</h1>
+        <h1 className="text-2xl font-bold mb-4">Comentatios del Post numero {id}</h1>
         <div className="flex flex-wrap">
       {post.map((coment) => (
         <div key={coment.id} className="w-full md:w-1/2 lg:w-1/3 p-4">
           <div className="bg-white rounded-lg shadow-lg">
             <div className="p-4">
               <h2 className="text-xl font-bold">
-                {coment.id} {""}Autor <span> {coment.name}</span> 
+                {coment.id} {""} Autor: <span> {coment.name}</span> 
               </h2>
               <p className="text-gray-600">{coment.body}</p>
             </div>
